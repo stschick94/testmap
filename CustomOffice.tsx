df8327ded7d2a0220906b05b90acb2f536b5d277
Schick <stefan.schick@iao.fraunhofer.de>
2021-04-11 11:42:54 +0200

    changed and edited map template

---
+++ CustomOffice.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="CustomOffice" tilewidth="32" tileheight="32" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../../../AppData/Local/Temp/New Piskel.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../../../Downloads/New Piskel.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../../../Downloads/New Piskel(1).png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../../../Downloads/New Piskel(2).png"/>
+ </tile>
+</tileset>

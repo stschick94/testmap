52cae3067e47aab5fc3f7fa0e615f1b9008db772
stschick94 <stefan.schick+github@posteo.de>
2021-04-11 14:13:44 +0000

    Deploying to gh-pages from master @ 9eab0c669da7743757d11a222416228cefeef95d 🚀

--- WelcomeIAO.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="WelcomeIAO" tilewidth="128" tileheight="57" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="128" height="57" source="fraunhofer-iao-logo.png"/>
- </tile>
-</tileset>
